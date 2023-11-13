7b1357ae29bd88c3985c106ae2e936ee20e03491
taldoflemis <coderflemis@gmail.com>
2023-11-13 16:47:59 -0300

    improvement: update DataSourceDefinition to contain audit and fields with time

--- internal/datasource/datasource.go
+++ internal/datasource/datasource.go
@@ -1,5 +1,10 @@
 package datasource
 
+import (
+	"fmt"
+	"time"
+)
+
 type DataSourceDefinition struct {
 	AdministrativeDependency string `json:"administrative_dependency"`
 	ExhibitionName           string `json:"exhibition_name"`
@@ -8,4 +13,57 @@ type DataSourceDefinition struct {
 	Source                   string `json:"source"`
 	TableName                string `json:"table_name"`
 	UpdateFrequency          string `json:"update_frequency"`
+}
+
+type DataSourceMetadataModel struct {
+	AdministrativeDependency string
+	ExhibitionName           string
+	LastDataCollection       time.Time
+	LastSourceUpdate         time.Time
+	Source                   string
+	TableName                string
+	UpdateFrequency          string
+	CreationDate             time.Time
+	UpdateDate               time.Time
+}
+
+func (d *DataSourceDefinition) parseMultipleDataTypes(dateString string) (time.Time, error) {
+	layouts := []string{
+		"2006-01-02",
+		"2006-01-02T15:04:05.000Z",
+		"2006-01-02 15:04:05",
+		"02/01/2006",
+	}
+
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, dateString)
+		if err == nil {
+			return t, nil
+		}
+	}
+
+	return time.Time{}, fmt.Errorf("no matching layout for date: %s", dateString)
+}
+
+func (d *DataSourceDefinition) ToDataSourceMetadateModel() (*DataSourceMetadataModel, error) {
+	lastDataCollection, err := time.Parse("2006-01-02 15:04:05", d.LastDataCollection)
+	if err != nil {
+		return nil, err
+	}
+	lastUpdate, err := d.parseMultipleDataTypes(d.LastUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataSourceMetadataModel{
+		AdministrativeDependency: d.AdministrativeDependency,
+		ExhibitionName:           d.ExhibitionName,
+		LastDataCollection:       lastDataCollection,
+		LastSourceUpdate:         lastUpdate,
+		Source:                   d.Source,
+		TableName:                d.TableName,
+		UpdateFrequency:          d.UpdateFrequency,
+		CreationDate:             time.Now(),
+		UpdateDate:               time.Now(),
+	}, nil
 }
